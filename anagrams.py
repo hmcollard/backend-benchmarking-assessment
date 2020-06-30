812e0fcdd175b47371263c835b219836f12af37a
Haley Collard <collard.haley20@gmail.com>
2020-06-30 14:37:02 -0400

    passing tests

--- anagrams.py
+++ anagrams.py
@@ -10,9 +10,11 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Haley Collard"
 
 import sys
+import cProfile
+import re
 
 
 def alphabetize(string):
@@ -27,11 +29,12 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        if alphabetize(word) not in anagrams:
+            anagrams[alphabetize(word)] = [word]
+        else:
+            anagrams[alphabetize(word)].extend([word])
     return anagrams
 
 
